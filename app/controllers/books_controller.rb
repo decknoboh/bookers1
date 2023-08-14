7b1b6b8b9997414b1bba9b55230a9244336188c2
decknoboh <135430121+decknoboh@users.noreply.github.com>
2023-08-14 05:04:51 +0000

    bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,20 +24,23 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.new(book_params)
+    @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book_id)
+      redirect_to book_path(@book.id)
     else
-      @books = Book.all
       render :edit
     end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully updated."
+      redirect_to '/books'
+    else
+      redirect_to '/books'
+    end
   end
   
   private
